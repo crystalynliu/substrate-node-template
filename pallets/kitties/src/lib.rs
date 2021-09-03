0277640525a0af050d219c242f0f422dd8d1ca5a
crystalyn <crystalynliu@163.com>
2021-09-04 00:15:24 +0800

    Implement the breed kitty and unit test

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -51,13 +51,15 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		KittyCreated(T::AccountId, KittyIndex),
 		KittyTransfer(T::AccountId, T::AccountId, KittyIndex),
+		KittyBreed(KittyIndex, KittyIndex)
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
 		KittiesCountOverflow,
 		NotKittyOwner,
-		KittiesNotExist,
+		KittyNotExist,
+		SameParentIndex
 	}
 
 	#[pallet::hooks]
@@ -97,13 +99,53 @@ pub mod pallet {
 		) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 
-			ensure!(Kitties::<T>::get(kitty_id).is_some(), Error::<T>::KittiesNotExist);
+			ensure!(Kitties::<T>::get(kitty_id).is_some(), Error::<T>::KittyNotExist);
 
 			ensure!(Some(who.clone()) == Owner::<T>::get(kitty_id), Error::<T>::NotKittyOwner);
 
 			Owner::<T>::insert(kitty_id, Some(new_owner.clone()));
 
 			Self::deposit_event(Event::KittyTransfer(who, new_owner, kitty_id));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(0)]
+		pub fn breed_kitty(
+			origin: OriginFor<T>,
+			kitty_id_1: KittyIndex,
+			kitty_id_2: KittyIndex
+		) -> DispatchResultWithPostInfo {
+			let who = ensure_signed(origin)?;
+
+			ensure!(kitty_id_1 != kitty_id_2, Error::<T>::SameParentIndex);
+
+			let kitty_1 = Self::kitties(kitty_id_1).ok_or(Error::<T>::KittyNotExist)?;
+			let kitty_2 = Self::kitties(kitty_id_2).ok_or(Error::<T>::KittyNotExist)?;
+
+			let kitty_id = match Self::kitties_count() {
+				Some(id) => {
+					ensure!(id != KittyIndex::max_value(), Error::<T>::KittiesCountOverflow);
+					id
+				},
+				None => { 0 }
+			};
+
+			let dna_1 = kitty_1.0;
+			let dna_2 = kitty_2.0;
+
+			let selector = Self::random_value(&who);
+			let mut new_dna = [0u8; 16];
+
+			for i in 0..dna_1.len() {
+				new_dna[i] = (selector[i] & dna_1[i]) | (!selector[i] & dna_2[i]);
+			}
+
+			Kitties::<T>::insert(kitty_id, Some(Kitty(new_dna)));
+			Owner::<T>::insert(kitty_id, Some(who.clone()));
+			KittiesCount::<T>::put(kitty_id + 1);
+
+			Self::deposit_event(Event::KittyBreed(kitty_id_1, kitty_id_2));
 
 			Ok(().into())
 		}
--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -40,7 +40,7 @@ fn transfer_kitty_failed_when_kitty_not_exist() {
 	new_test_ext().execute_with( || {
 		assert_noop!(
 			KittiesModule::transfer_kitty(Origin::signed(123), 456, 0),
-			Error::<Test>::KittiesNotExist
+			Error::<Test>::KittyNotExist
 		);
 	})
 }
@@ -53,6 +53,60 @@ fn transfer_kitty_failed_when_not_kitty_owner() {
 		assert_noop!(
 			KittiesModule::transfer_kitty(Origin::signed(1), 456, 0),
 			Error::<Test>::NotKittyOwner
+		);
+	})
+}
+
+#[test]
+fn breed_kitty_works() {
+	new_test_ext().execute_with( || {
+		let _ = KittiesModule::create_kitty(Origin::signed(123));
+		let _ = KittiesModule::create_kitty(Origin::signed(123));
+
+		assert_ok!(KittiesModule::breed_kitty(Origin::signed(123), 0, 1));
+		assert_eq!(KittiesCount::<Test>::get(), Some(3));
+		assert!(Kitties::<Test>::get(2).is_some());
+		assert_eq!(Owner::<Test>::get(2), Some(123));
+	})
+}
+
+#[test]
+fn breed_kitty_failed_when_parent_index_is_same() {
+	new_test_ext().execute_with( || {
+		let _ = KittiesModule::create_kitty(Origin::signed(123));
+		let _ = KittiesModule::create_kitty(Origin::signed(123));
+
+		assert_noop!(
+			KittiesModule::breed_kitty(Origin::signed(123), 1, 1),
+			Error::<Test>::SameParentIndex
+		);
+	})
+}
+
+#[test]
+fn breed_kitty_failed_when_kitty1_not_exist() {
+	new_test_ext().execute_with( || {
+		let _ = KittiesModule::create_kitty(Origin::signed(123));
+
+		assert!(Kitties::<Test>::get(0).is_some());
+		assert!(Kitties::<Test>::get(1).is_none());
+		assert_noop!(
+			KittiesModule::breed_kitty(Origin::signed(123), 0, 1),
+			Error::<Test>::KittyNotExist
+		);
+	})
+}
+
+#[test]
+fn breed_kitty_failed_when_kitty2_not_exist() {
+	new_test_ext().execute_with( || {
+		let _ = KittiesModule::create_kitty(Origin::signed(123));
+
+		assert!(Kitties::<Test>::get(0).is_some());
+		assert!(Kitties::<Test>::get(1).is_none());
+		assert_noop!(
+			KittiesModule::breed_kitty(Origin::signed(123), 1, 0),
+			Error::<Test>::KittyNotExist
 		);
 	})
 }
